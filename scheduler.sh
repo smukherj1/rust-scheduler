0af2e06a40ef8222dac104ebbc358714ade8dd17
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2024-10-14 12:11:20 -0400

    Add metrics boilerplate

--- scheduler.sh
+++ scheduler.sh
@@ -1,4 +1,4 @@
 #/usr/bin/bash
 
 set -eu
-cargo run --release --bin scheduler -- -a "0.0.0.0:50051"
+cargo run --release --bin scheduler -- -a "0.0.0.0:50051" -m "0.0.0.0:9090"
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,8 +1,12 @@
 mod queue;
 
+use std::sync::Arc;
+
 use anyhow::{Context, Result};
+use axum::Router;
 use clap::Parser;
 use once_cell::sync::Lazy;
+use prometheus::{self, register_int_counter_vec, Encoder, IntCounterVec, TextEncoder};
 use queue::scheduler;
 use scheduler::builds_server::{Builds, BuildsServer};
 use scheduler::workers_server::{Workers, WorkersServer};
@@ -13,12 +17,34 @@ use scheduler::{
 };
 use tonic::{transport::Server, Request, Response, Status};
 
+static BUILDS_STARTED_COUNT: Lazy<IntCounterVec> = Lazy::new(|| {
+    register_int_counter_vec!(
+        "builds_started_count",
+        "Number of builds started",
+        &["status"]
+    )
+    .unwrap()
+});
+
+static BUILDS_COMPLETED_COUNT: Lazy<IntCounterVec> = Lazy::new(|| {
+    register_int_counter_vec!(
+        "builds_completed_count",
+        "Number of completed builds",
+        &["status"]
+    )
+    .unwrap()
+});
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// URL for the address this scheduler GRPC server will serve at.
     #[arg(short, long)]
     address: String,
+
+    /// URL for the address to serve metrics at.
+    #[arg(short, long)]
+    metrics_address: String,
 }
 
 pub struct BuildsService<'a> {
@@ -37,15 +63,25 @@ impl Builds for BuildsService<'static> {
         &self,
         request: Request<CreateBuildRequest>,
     ) -> Result<Response<CreateBuildResponse>, Status> {
+        let report_metric = |c: tonic::Code| {
+            BUILDS_STARTED_COUNT
+                .with_label_values(&[format!("{c}").as_str()])
+                .inc();
+        };
         let b = request
             .into_inner()
             .build
             .ok_or(tonic::Status::invalid_argument(
                 "field build can't be missing in create_build",
-            ))?;
-        let build_id = self.q.create_build(b)?;
+            ))
+            .inspect_err(|st| report_metric(st.code()))?;
+        let build_id = self
+            .q
+            .create_build(b)
+            .inspect_err(|st| report_metric(st.code()))?;
         let resp = CreateBuildResponse { build_id };
 
+        BUILDS_STARTED_COUNT.with_label_values(&["ok"]).inc();
         Ok(Response::new(resp)) // Send back our formatted greeting
     }
 
@@ -66,6 +102,11 @@ impl Builds for BuildsService<'static> {
                 ))
             })?,
         };
+        BUILDS_COMPLETED_COUNT
+            .with_label_values(
+                &[format!("{}", tonic::Code::from_i32(build_result.status)).as_str()],
+            )
+            .inc();
         Ok(Response::new(WaitBuildResponse {
             build_result: Some(build_result),
         }))
@@ -140,30 +181,96 @@ impl Workers for WorkersService<'static> {
     }
 }
 
+async fn metrics() -> Result<String, axum::http::StatusCode> {
+    let encoder = TextEncoder::new();
+    let mut buffer = vec![];
+    encoder
+        .encode(&prometheus::gather(), &mut buffer)
+        .expect("Failed to encode metrics");
+
+    let response = String::from_utf8(buffer).expect("Failed to convert bytes to string");
+    Ok(response)
+}
+
+async fn launch_metrics_server(args: Arc<Args>) -> Result<()> {
+    let app = Router::new().route("/metrics", axum::routing::get(metrics));
+    let listener = tokio::net::TcpListener::bind(args.metrics_address.as_str())
+        .await
+        .with_context(|| {
+            format!(
+                "error initializing listener for metrics server at address {}",
+                args.metrics_address.as_str()
+            )
+        })?;
+    println!(
+        "Launching scheduler metrics server on {}/metrics",
+        args.metrics_address
+    );
+    axum::serve(listener, app).await.with_context(|| {
+        format!(
+            "metrics server at address {} crashed",
+            args.metrics_address.as_str()
+        )
+    })?;
+    Ok(())
+}
+
+async fn launch_scheduler_server(args: Arc<Args>) -> Result<()> {
+    let addr = args.address.parse().with_context(|| {
+        format!(
+            "failed to parse {} as a socker address for scheduler GRPC server",
+            args.address
+        )
+    })?;
+    static Q: Lazy<queue::Queue> = Lazy::new(queue::Queue::new);
+    let bs = BuildsService::new(&Q);
+    let ws = WorkersService::new(&Q);
+    println!("Launching scheduler GRPC server on {:?}", addr);
+    Server::builder()
+        .add_service(BuildsServer::new(bs))
+        .add_service(WorkersServer::new(ws))
+        .serve(addr)
+        .await
+        .with_context(|| format!("failed to launch GRPC server on {:?}", addr))?;
+    Ok(())
+}
+
 fn validate_args(args: &Args) -> Result<()> {
     if args.address.is_empty() {
         anyhow::bail!("address flag can't be empty");
+    }
+    if args.metrics_address.is_empty() {
+        anyhow::bail!("metrica_address flag can't be empty");
     }
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+    let args = Arc::new(Args::parse());
     validate_args(&args).with_context(|| "failed to validate command line arguments")?;
-    let addr = args
-        .address
-        .parse()
-        .with_context(|| format!("failed to parse {} as a socker address", args.address))?;
-    static Q: Lazy<queue::Queue> = Lazy::new(queue::Queue::new);
-    let bs = BuildsService::new(&Q);
-    let ws = WorkersService::new(&Q);
-    println!("Launching server on {:?}", addr);
-    Server::builder()
-        .add_service(BuildsServer::new(bs))
-        .add_service(WorkersServer::new(ws))
-        .serve(addr)
-        .await?;
+
+    let mut js = tokio::task::JoinSet::new();
+
+    let margs = Arc::clone(&args);
+    js.spawn(launch_metrics_server(margs));
+    js.spawn(launch_scheduler_server(args));
+
+    while let Some(res) = js.join_next().await {
+        let res = match res {
+            Ok(r) => r,
+            Err(err) => {
+                eprintln!("Shutting down due to error joining spawned server threads: {err:?}");
+                js.shutdown().await;
+                break;
+            }
+        };
+        if let Err(err) = res {
+            eprintln!("Shutting down due to error from a spawned server thread: {err:?}");
+            js.shutdown().await;
+            break;
+        }
+    }
 
     Ok(())
 }
